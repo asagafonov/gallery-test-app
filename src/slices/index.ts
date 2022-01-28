88661307cf94e9248ab8a3741f4edf7731fd7d9b
asagafonov <a.s.agafonov@yandex.ru>
2022-01-28 11:16:08 +0300

    add store

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cardsReducer from './cardsReducer';
 
-export default configureStore({
+const store = configureStore({
   reducer: {
-    cards: cardsReducer,
+    cardsContainer: cardsReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>
+
+export default store;
