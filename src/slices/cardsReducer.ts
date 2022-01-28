8af12f4e35896a9843bff9583e51c116b618d985
asagafonov <a.s.agafonov@yandex.ru>
2022-01-28 11:16:23 +0300

    add reducers

--- src/slices/cardsReducer.ts
+++ src/slices/cardsReducer.ts
@@ -1,20 +1,63 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
+import { Card, CardsState } from '../utils/interfaces';
+import { getImageURL } from '../utils/urls';
+
+const initialState: CardsState = {
   cards: [],
+  filterIsOn: false,
 };
 
 export const cardsSlice = createSlice({
   name: 'cards',
   initialState,
   reducers: {
-    setActiveFramework: (state, action) => {
-      const { id } = action.payload;
-      state.activeFrameworkIndex = id;
+    setCardsData: (state, action) => {
+      const { data } = action.payload;
+
+      const dataWithLikes = data
+        .map((item: Card): Card => ({
+          ...item,
+          url: getImageURL(item.image_id),
+          hasLike: false,
+        }));
+
+      state.cards = dataWithLikes;
+    },
+    removeCard: (state, action) => {
+      const { cardId } = action.payload;
+
+      state.cards = state.cards
+        .filter((item: Card): Boolean => item.id !== cardId);
+    },
+    addLike: (state, action) => {
+      const { cardId } = action.payload;
+
+      state.cards = state.cards.map((item: Card): Card => {
+        if (item.id === cardId) {
+          return { ...item, hasLike: true };
+        }
+        return item;
+      });
+    },
+    removeLike: (state, action) => {
+      const { cardId } = action.payload;
+
+      state.cards = state.cards.map((item: Card): Card => {
+        if (item.id === cardId) {
+          return { ...item, hasLike: false };
+        }
+        return item;
+      });
+    },
+    switchFilter: (state) => {
+      state.filterIsOn = !state.filterIsOn;
     },
   },
 });
 
-export const { setActiveFramework } = cardsSlice.actions;
+export const {
+  setCardsData, removeCard, addLike, removeLike, switchFilter,
+} = cardsSlice.actions;
 
 export default cardsSlice.reducer;
