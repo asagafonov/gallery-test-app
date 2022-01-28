ae0570738420dc55f7c041e396cb4e4cf3987201
asagafonov <a.s.agafonov@yandex.ru>
2022-01-28 13:54:37 +0300

    upd number of items loaded

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,4 +1,4 @@
-const getItemsURL: string = 'https://api.artic.edu/api/v1/artworks/search?query[term][is_public_domain]=true&limit=21&fields=id,title,image_id';
+const getItemsURL: string = 'https://api.artic.edu/api/v1/artworks/search?query[term][is_public_domain]=true&limit=60&fields=id,title,image_id';
 const getImageURL = (id: number): string => `https://www.artic.edu/iiif/2/${id}/full/843,/0/default.jpg`;
 
 export { getItemsURL, getImageURL };
